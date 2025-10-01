6ed9c05e545fcdf0b2f3a3a3cfb8ac56a5e3f09c
myyrakle <sssang97@naver.com>
2025-10-02 01:22:09 +0900

    [#4] implement: WalRecordJsonCodec

--- src/errors.rs
+++ src/errors.rs
@@ -1,4 +1,7 @@
 #[derive(Debug)]
-pub enum Errors {}
+pub enum Errors {
+    WalRecordEncodeError(String),
+    WalRecordDecodeError(String),
+}
 
 pub type Result<T> = std::result::Result<T, Errors>;
