b723ba1da34af5f46c52e314d7e0a36bb07381d8
myyrakle <sssang97@naver.com>
2025-10-02 01:22:09 +0900

    [#4] implement: WalRecordJsonCodec

--- src/wal.rs
+++ src/wal.rs
@@ -7,19 +7,30 @@ pub struct WalRecord {
     pub data: serde_json::Value,
 }
 
-pub trait WalCodec {
-    fn encode(&self) -> errors::Result<Vec<u8>>;
-    fn decode(data: &[u8]) -> errors::Result<Self>
-    where
-        Self: Sized;
-}
-
 #[derive(Debug, Clone, PartialEq, serde::Serialize, serde::Deserialize)]
 pub enum RecordType {
     #[serde(rename = "put")]
     Put,
     #[serde(rename = "delete")]
     Delete,
+}
+
+pub trait WalRecordCodec {
+    fn encode(&self, record: &WalRecord) -> errors::Result<Vec<u8>>;
+    fn decode(&self, data: &[u8]) -> errors::Result<WalRecord>;
+}
+
+pub struct WalRecordJsonCodec;
+
+impl WalRecordCodec for WalRecordJsonCodec {
+    fn encode(&self, record: &WalRecord) -> errors::Result<Vec<u8>> {
+        serde_json::to_vec(record).map_err(|e| errors::Errors::WalRecordEncodeError(e.to_string()))
+    }
+
+    fn decode(&self, data: &[u8]) -> errors::Result<WalRecord> {
+        serde_json::from_slice(data)
+            .map_err(|e| errors::Errors::WalRecordDecodeError(e.to_string()))
+    }
 }
 
 pub const WAL_SEGMENT_SIZE: usize = 1024 * 1024 * 16; // 16MB
