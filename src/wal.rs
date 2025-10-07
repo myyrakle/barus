5edfba4a62fa990ff72b2b71486132ba08ad0f8c
myyrakle <sssang97@naver.com>
2025-10-07 15:56:58 +0900

    [#4] define WalCodec trait

--- src/wal.rs
+++ src/wal.rs
@@ -1,8 +1,17 @@
+use crate::errors;
+
 #[derive(Debug, Clone, PartialEq, serde::Serialize, serde::Deserialize)]
 pub struct WalRecord {
     pub record_id: u64,
     pub record_type: RecordType,
     pub data: serde_json::Value,
+}
+
+pub trait WalCodec {
+    fn encode(&self) -> errors::Result<Vec<u8>>;
+    fn decode(data: &[u8]) -> errors::Result<Self>
+    where
+        Self: Sized;
 }
 
 #[derive(Debug, Clone, PartialEq, serde::Serialize, serde::Deserialize)]
@@ -12,3 +21,5 @@ pub enum RecordType {
     #[serde(rename = "delete")]
     Delete,
 }
+
+pub const WAL_SEGMENT_SIZE: usize = 1024 * 1024 * 16; // 16MB
